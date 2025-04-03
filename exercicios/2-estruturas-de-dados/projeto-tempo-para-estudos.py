b2126706b92fbfb5950da7cae3f5552d514f57f8
AldBer <139921230+AldBer@users.noreply.github.com>
2025-04-03 19:46:35 +0000

    Resol_Pjt_01

--- exercicios/2-estruturas-de-dados/projeto-tempo-para-estudos.py
+++ exercicios/2-estruturas-de-dados/projeto-tempo-para-estudos.py
@@ -4,3 +4,11 @@
 # 3. Crie uma variável chamada "total_horas" e, usanod o método input(), solicite a média de horas estudada por dia;
 # 4. Crie uma variável chamada "curso" e, usando o método input(), solicite o título do curso desejado;
 # 5. Imprima na tela uma frase informando o nome da estudante, o total_dias dedicados aos estudos, o total horas semanais e o curso.
+nome = input('Qual seu nome?')
+total_dias = input('Quantos dias na semana você dedica ao estudo?')
+total_horas = input('Quantas horas por + / - você estuda por dia?')
+curso = input('Qual curso você está estudando?')
+total_dias = int(total_dias)
+total_horas = int(total_horas)
+
+print (f'Olá {nome}, você costua estudar {total_dias} dias por semana,durante aproximadamente {total_horas} horas diarias. Isso significa que você estuda em média {total_dias * total_horas} horas por semana do curso de {curso}.')
