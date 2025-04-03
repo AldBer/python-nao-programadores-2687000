47beeb617543082cf3e89ac7d1c65fbb67b0cafa
AldBer <139921230+AldBer@users.noreply.github.com>
2025-04-03 18:44:48 +0000

    Rel_Ex01_02

--- exercicios/2-estruturas-de-dados/1-lista.py
+++ exercicios/2-estruturas-de-dados/1-lista.py
@@ -1,13 +1,16 @@
 # Crie uma lista apenas com elementos numéricos
-
+Cascata = [1,3,5,7,9]
+print (Cascata)
 # Crie uma lista contendo todos os tipos e estrutura de dados que você aprendeu até agora
-
+Est_dados = ['string','variaveis','numericos','inteiros','flutuante','boleanos','logicos']
+print (Est_dados)
 # Imprima na tela apenas os 5 primeiros elementos da lista
-
+print(Est_dados[:5])
 # Crie um slice na lista para que imprima na tela os elementos de índice par
-
+print(Est_dados[0:-1:2])
 # Remova da lista o último item
-
+Est_dados.pop()
 # Insira na lista um novo item
-
+Est_dados.append('operadores')
 # Remova da lista um item específico
+Est_dados.remove('flutuante')
