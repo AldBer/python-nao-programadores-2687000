a68ffa2a8686c6b5672da1eafc269a8a0c2c213a
Aldo Bernardi <139921230+AldBer@users.noreply.github.com>
2025-04-07 18:28:13 +0000

    Resol_Ex

--- exercicios/3-estruturas-de-controle/projeto-avaliacao-cursos.py
+++ exercicios/3-estruturas-de-controle/projeto-avaliacao-cursos.py
@@ -5,3 +5,18 @@
 # 4. Crie uma estrutura condicional para verificar se cada variável está contida na lista
 # 5. Se o curso estiver na lista, solicite uma nota para avaliação
 # 6. Armazene essa nota no dicionário, sendo a chave o título do curso e o valor a nota
+cursos_Linkedin = ['JavaSript','R','SQL','Python','C++']
+curso_A = 'JavaScript'
+curso_B = 'SQL'
+curso_C = 'Python'
+rank = {}
+if curso_A in cursos_Linkedin:
+  print(f"O curso {curso_A} está no catalogo. Por favor, avalie o curso.")
+  rank[curso_A]= int(input('Qual a nota que você avalia o curso (0 a 5)?'))
+if curso_B in cursos_Linkedin:
+  print(f"O curso {curso_B} está no catalogo. Por favor, avalie o curso.")
+  rank[curso_B]= int(input('Qual a nota que você avalia o curso (0 a 5)?'))
+if curso_C in cursos_Linkedin:
+  print(f"O curso {curso_C} está no catalogo. Por favor, avalie o curso.")
+  rank[curso_C]= int(input('Qual a nota que você avalia o curso (0 a 5)?'))
+print(rank)
